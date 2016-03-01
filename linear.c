b98dd78b262a519df8d8034bba617d645eb609af
Odysseas <OdysseasKr@users.noreply.github.com>
2016-03-01 12:08:13 +0200

    File parsing restructure

--- linear.c
+++ linear.c
@@ -1,70 +1,77 @@
-/*
- * Date: February 29 2016
- * Author: Poiitis Marinos
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 
 int countlines(FILE *file);
+int readFile(char *fname, double ***coords);
+void freeCoords(double ***coords, int no_of_lines);
 
 int main(int argc, char *argv[]) {
-
-    FILE *fp;
-
-    fp = fopen(argv[3],"r");//read only
-
-    int no_of_lines = countlines(fp);
-    rewind(fp);//sends pointer at the beggining
-    double **coords;
-
-    coords = (double **)malloc(no_of_lines*sizeof(double *));
-    int k;
-    for(k=0;k<no_of_lines;k++){
-        coords[k] = (double *)malloc(3*sizeof(double));
-    }
-
-
-    if(fp == NULL) {
-        fprintf(stderr,"Error while opening file %s", argv[3]);
-        return 0;
-    }
-
-
-    int i;
-
-    for(i=0;i<no_of_lines;i++){
-        fscanf(fp,"%lf",&coords[i][0]);
-        fscanf(fp,"%lf",&coords[i][1]);
-        fscanf(fp,"%lf",&coords[i][2]);
-
-    }
-
+    double **coords = NULL;
+    int no_of_lines = readFile(argv[3], &coords);
+    
     int j;
-    for(j=0;j<no_of_lines;j++){
+    for(j=0;j<no_of_lines;j++) {
         printf("%f \n" , coords[j][0]);
         printf("%f \n" , coords[j][1]);
         printf("%f \n" , coords[j][2]);
     }
 
-    fclose(fp);
-
+    freeCoords(&coords, no_of_lines);
     return 0;
 }
 
-int countlines(FILE *file){
+int countlines(FILE *file) {
 
     char c;
     int counter=0;
 
     c=fgetc(file);
-    while(c != EOF){
+    while(c != EOF) {
         if(c == '\n')
             counter++;
         c=fgetc(file);
     }
-
+    
+    rewind(file);//sends pointer at the beggining
     return counter;
 }
 
+int readFile(char *fname, double ***coords) {
+    FILE *fp;
+
+    fp = fopen(fname,"r");//read only
+    
+    if(fp == NULL) {
+        fprintf(stderr,"Error while opening file %s", fname);
+        return 0;
+    }
+
+    int no_of_lines = countlines(fp);
+    double ** temp;   
+    temp = (double **)malloc(no_of_lines*sizeof(double *));
+    int k;
+    for(k=0;k<no_of_lines;k++) {
+        temp[k] = (double *)malloc(3*sizeof(double));
+    }
+
+    int i;
+    for(i=0;i<no_of_lines;i++) {
+        fscanf(fp,"%lf",&temp[i][0]);
+        fscanf(fp,"%lf",&temp[i][1]);
+        fscanf(fp,"%lf",&temp[i][2]);
+    }
+    *coords = temp;
+
+    fclose(fp);
+
+    return no_of_lines;
+}
+
+void freeCoords(double ***coords, int no_of_lines) {
+    int j=0;
+    for (j=0;j<no_of_lines;j++) {
+        free((*coords)[j]);
+    }
+    free(*coords);   
+}
