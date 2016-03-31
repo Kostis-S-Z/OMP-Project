433f3eb3c50548581d4a7310532b00cd8be7b720
OdysseasKr <OdysseasKr@users.noreply.github.com>
2016-04-01 00:14:23 +0300

    Add more data

--- Utilities/plot.sh
+++ Utilities/plot.sh
@@ -8,14 +8,14 @@ set output "times.png"
 unset key
 
 set yrange [0:11]
-set xrange [-0.1:4]
+set xrange [-0.1:5.5]
 
 set boxwidth 0.1
 set style fill solid
 
 set title "Running times for 15 million collisions"
 set ylabel "Seconds"
-set xtics ("Linear" 0.5, "Using OMP" 2, "Using OMP and MPI" 3.5)
+set xtics ("Linear" 0.5, "Using OMP" 2, "Using MPI" 3.5, "Using OMP and MPI" 5)
 set ytics 0,1,11
 
-plot 'lineardata.dat' with boxes lt rgb "red", 'ompdata.dat' with boxes lt rgb "orange", 'paralleldata.dat' with boxes lt rgb "green"
+plot 'lineardata.dat' with boxes lt rgb "red", 'ompdata.dat' with boxes lt rgb "orange", 'mpidata.dat' with boxes lt rgb "green", 'paralleldata.dat' with boxes lt rgb "blue"
